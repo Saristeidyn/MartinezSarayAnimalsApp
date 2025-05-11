8e6ad762c75646a2b80bac5826384e61689b7b33
saray <sm78544@lasallebajio.edu.mx>
2025-05-11 15:00:44 -0600

    Started to make the navigation

--- app/src/main/java/com/planet/martinezsarayanimalsapp/MainActivity.kt
+++ app/src/main/java/com/planet/martinezsarayanimalsapp/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.planet.martinezsarayanimalsapp.ui.theme.MartinezSarayAnimalsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +30,45 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MartinezSarayAnimalsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar(
+                            containerColor = Color(0xFFFFF8DC),
+                            tonalElevation = 4.dp,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+                        ) {
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = { /* Navigate to Animals */ },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Home,
+                                        contentDescription = "Inicio",
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                },
+                                label = { Text("Inicio") },
+                                alwaysShowLabel = true
+                            )
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /* Navigate to Ambientes */ },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Home,
+                                        contentDescription = "Ambiente",
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                },
+                                label = { Text("Ambiente") },
+                                alwaysShowLabel = true
+                            )
+                        }
+
+                    }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
